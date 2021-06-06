cb3104bd29acf6d5ae4e476875263176c0c72229
Michelle <michellehnguyen03@gmail.com>
2021-06-06 13:57:45 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+     user_input = input("Please type in an acronym of a weekday: ")
+
+     user_input = user_input.lower()
+
+     if (user_input == "mon"):
+         print("Monday")
+     elif (user_input == "tue"):
+         print("Tuesday")
+     elif (user_input == "wed"):
+         print("Wednesday")
+     elif (user_input == "thur"):
+         print("Thursday")
+     elif (user_input == "fri"):
+         print("Friday")
+     else:
+         print("Incorrect acronym")
+
+
+if __name__ == "__main__":
+    main()
+
+
